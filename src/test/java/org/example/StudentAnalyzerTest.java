a9c30180a11399d5f94823abe19fb23fdb3ea454
NgoDucViet <ngdviet2225@gmail.com>
2026-02-28 12:46:59 +0700

    test: add unit tests for both methods #3

--- src/test/java/org/example/StudentAnalyzerTest.java
+++ src/test/java/org/example/StudentAnalyzerTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
 class StudentAnalyzerTest {
 
     private final StudentAnalyzer analyzer = new StudentAnalyzer();
